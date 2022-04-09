6fa36c3e3febfd6bb8c384f6d3519b7c3f193bd4
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-09 17:38:30 -0400

    Added links on root page

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.0.3'
-
+#Adding the faker gem to seed data for challenge
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.5'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+10.times do
+  title = Faker::Beer.name
+  content = Faker::Lorem.words(number: 20)
+  Article.create(title: title, content: content)
+end
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
