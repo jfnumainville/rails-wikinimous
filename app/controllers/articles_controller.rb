35a726dcab48e3fa887478e0c092187261086ae0
Jean-Francois Numainville <jfnumainville@gmail.com>
2022-04-09 19:10:07 -0400

    Finished the Rail Wikinimous challenge

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,36 @@ class ArticlesController < ApplicationController
 
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    redirect_to root_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to root_path
+  end
+
   private
 
   def set_article
   @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :content)
   end
 end
